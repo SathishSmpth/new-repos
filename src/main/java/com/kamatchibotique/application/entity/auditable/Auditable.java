a746cbc95bcc8019ed22c67d500c65ebb825a7aa
Sathish Sampath <sathishsmpth@outlook.com>
2024-06-14 22:08:41 +0530

    msg

--- src/main/java/com/kamatchibotique/application/entity/auditable/Auditable.java
+++ src/main/java/com/kamatchibotique/application/entity/auditable/Auditable.java
@@ -1,8 +1,13 @@
 package com.kamatchibotique.application.entity.auditable;
 
 import org.hibernate.annotations.CreationTimestamp;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedBy;
+import org.springframework.data.annotation.LastModifiedDate;
 
-public class Auditable {
+import java.time.LocalDateTime;
+
+public class Auditable<U> {
     @CreationTimestamp
     protected U createdBy;
 
